05ed985971b000290d01c2f2006c1d75ee98a581
beerpsi <lacvtg.a1.2023@gmail.com>
2021-12-28 12:36:00 +0700

    Cleaner code, hopefully

--- ci/main.py
+++ ci/main.py
@@ -18,6 +18,7 @@ def main():
     __scriptdir = os.path.dirname(os.path.realpath(__file__))
     bypasses_file = os.path.join(__scriptdir, '..', 'manifests', 'bypasses.yaml')
     apps_dir = os.path.join(__scriptdir, '..', 'manifests', 'apps')
+    appstorepp_repo = 'https://cokepokes.github.io'
 
     with open(bypasses_file, encoding='utf-8') as file:
         bypasses = yaml.safe_load(file)
@@ -28,40 +29,42 @@ def main():
     for app_file in apps_files:
         with open(app_file, encoding='utf-8') as file:
             app = yaml.safe_load(file.read())
-        if app['bypasses'] is None:
-            table_matrix.append([f"[{app['name']}]({app['uri']})", '', 'No known bypass', ''])
-            continue
-        
-        bypass_versions = str()
-        bypass_tweaks = str()
-        bypass_notes = str()
+
+        app_name = markdown_link(app['name'], app['uri'])        
+        bypass_versions = list()
+        bypass_tweaks = list()
+        bypass_notes = list()
         downgrade_noted = False
-        for bypass in app['bypasses']:
-            bypass_versions += f"{bypass['version']}<br>" if 'version' in bypass else '<br>'
+        if app['bypasses'] is None:
+            bypass_versions.append('')
+            bypass_tweaks.append('No known bypass')
+            bypass_notes.append('')
+        else:
+            for bypass in app['bypasses']:
+                bypass_versions.append(f"{bypass['version']}" if 'version' in bypass else '')
 
-            if 'name' in bypass:
-                bypass_tweak = f"{markdown_link(bypass['name'], bypasses[bypass['name']]['guide'])}" if 'guide' in bypasses[bypass['name']] else bypass['name']
-                bypass_tweak_repo = f" ({markdown_link('repo', bypasses[bypass['name']]['repo'], sharerepo=True)})" if 'repo' in bypasses[bypass['name']] else None
-                bypass_tweaks += bypass_tweak + bypass_tweak_repo + '<br>'
+                if 'name' in bypass:
+                    bypass_tweak = f"{markdown_link(bypass['name'], bypasses[bypass['name']]['guide'])}" if 'guide' in bypasses[bypass['name']] else bypass['name']
+                    bypass_tweak_repo = f" ({markdown_link('repo', bypasses[bypass['name']]['repo'], sharerepo=True)})" if 'repo' in bypasses[bypass['name']] else None
+                    bypass_tweaks.append(bypass_tweak + bypass_tweak_repo)
 
-                if not downgrade_noted and 'version' in bypass and bypass['name'] != "AppStore++":
-                    notes_from_version = f"- Use AppStore++ ({markdown_link('repo', 'https://cokepokes.github.io', sharerepo=True)}) to downgrade.<br>"
-                    downgrade_noted = True 
-                else:
-                    notes_from_version = ''
-                notes_from_bypass = f"{bypasses[bypass['name']]['notes']} " if 'notes' in bypasses[bypass['name']] else ''
-                notes_from_manifest = bypass['notes'] if 'notes' in bypass else ''
-                bypass_notes += notes_from_version + (f"- **{bypass['name']}**: " + notes_from_bypass + notes_from_manifest + '<br>' if (notes_from_bypass or notes_from_manifest) else '')
-            elif 'notes' in bypass:
-                logger.warning('Bypass name not specified, printing notes only')
-                bypass_tweaks += ''
-                bypass_notes += f"- {bypass['notes']}<br>" if 'notes' in bypass else '<br>'
-            else: 
-                logger.error('Neither name nor notes were supplied for this bypass!')
-                continue  
-        table_matrix.append([f"[{app['name']}]({app['uri']})", bypass_versions, bypass_tweaks, bypass_notes])
+                    if not downgrade_noted and 'version' in bypass and bypass['name'] != "AppStore++":
+                        bypass_notes.append(f"- Use AppStore++ ({markdown_link('repo', appstorepp_repo, sharerepo=True)}) to downgrade.")
+                        downgrade_noted = True
+
+                    notes_from_bypass = f"{bypasses[bypass['name']]['notes']} " if 'notes' in bypasses[bypass['name']] else ''
+                    notes_from_manifest = bypass['notes'] if 'notes' in bypass else ''
+                    if notes_from_bypass or notes_from_manifest:
+                        bypass_notes.append(f"- **{bypass['name']}**: " + notes_from_bypass + notes_from_manifest)
+                elif 'notes' in bypass:
+                    logger.warning('Bypass name not specified, printing notes only')
+                    bypass_notes.append(f"- {bypass['notes']}")
+                else: 
+                    logger.error('Neither name nor notes were supplied for this bypass!')
+                    continue  
+        table_matrix.append([f"{app_name}", '<br>'.join(bypass_versions), '<br>'.join(bypass_tweaks), '<br>'.join(bypass_notes)])
+
     table_matrix.sort(key=lambda app: app[0].lower())
-
     writer = MarkdownTableWriter(
         headers=["App", "Version", "Bypass", "Notes"],
         value_matrix=table_matrix,
@@ -69,15 +72,15 @@ def main():
     )
 
     # Writing to the actual app-list.md
-    if not args.print:
+    if args.print:
+        writer.write_table()
+    else:
         headers_dir = os.path.join(__scriptdir, 'md-headers')
         app_list_md = os.path.join(__scriptdir, '..', 'app-list.md')
         with open(os.path.join(headers_dir, 'app-list.md'), encoding='utf-8', mode='r') as infile, open(app_list_md, encoding='utf-8', mode='w') as outfile:
             outfile.write(infile.read())
             outfile.write('\n\n')
             outfile.write(writer.dumps())
-    else:
-        writer.write_table()
 
 if __name__ == "__main__":
     main()

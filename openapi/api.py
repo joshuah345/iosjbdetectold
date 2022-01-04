057a2918ee3dbb07589b48234292e5d3fe180ee3
beerpsi <lacvtg.a1.2023@gmail.com>
2022-01-04 19:16:09 +0700

    attribute note to vnode, change workflow a bit

--- openapi/api.py
+++ openapi/api.py
@@ -7,7 +7,6 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
 from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 
 
 def return_results(list_of_dicts, query, threshold):
@@ -15,15 +14,14 @@ def return_results(list_of_dicts, query, threshold):
     scores = list()
     for index, item in enumerate(list_of_dicts):
         values = [item['name'].lower(), item['bundleId'].lower()]
-        partial_ratios = [fuzz.partial_ratio(str(query), str(value)) for value in values] # ensure both are in string
-        ratios = [fuzz.ratio(str(query), str(value)) for value in values]
+        ratios, partial_ratios = [fuzz.ratio(str(query), str(value)) for value in values], [fuzz.partial_ratio(str(query), str(value)) for value in values] # ensure both are in string
         scores.append({"index": index, "partial_score": max(partial_ratios), "score": max(ratios)})
 
     filtered_scores = [item for item in scores if item['score'] >= threshold or item['partial_score'] >= threshold]
     sorted_filtered_scores = sorted(filtered_scores, key = lambda k: (k['score'], k['partial_score']), reverse=True)
     if len(sorted_filtered_scores) > 0 and sorted_filtered_scores[0]['score'] == 100:
         return [list_of_dicts[sorted_filtered_scores[0]['index']]]
-    filtered_list_of_dicts = [ list_of_dicts[item["index"]] for item in sorted_filtered_scores ]
+    filtered_list_of_dicts = [list_of_dicts[item["index"]] for item in sorted_filtered_scores]
     return filtered_list_of_dicts
 
 
